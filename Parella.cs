86610843fc8eb4003ce75fb69db027950207aeb1
Unai Calvet <unai.calvet.7e8@itb.cat>
2025-02-05 11:35:59 +0100

    solució exercici 1 closese #1

---
+++ Parella.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T4Activitats {
+    public class Parella<T1, T2> {
+        public T1 PrimerValor { get; set; }
+        public T2 SegonValor { get; set; }
+        public Parella (T1 primerValor, T2 segonValor) {
+            PrimerValor = primerValor;
+            SegonValor = segonValor;
+        }
+        public void MostrarValors() {
+        Console.WriteLine("Primer Valor valor {0}, Segon Valor {1}", PrimerValor, SegonValor);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using T4Activitats;
+
+public class ActT4 {
+    public static void Main() {
+        //Exercici 1
+        Parella <string,int> parellaStringInt = new Parella<string, int>("text", 1);
+        Parella<double, string> parellaDoubleString = new Parella<double, string>(3.14, "text");
+    }
+}
