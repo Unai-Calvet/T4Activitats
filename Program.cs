8793ab57a031e36c97ba620a5a1ed6530c1bacc0
Unai Calvet <unai.calvet.7e8@itb.cat>
2025-02-05 12:04:06 +0100

    Solució Exercici 2 closes #4

--- Program.cs
+++ Program.cs
@@ -1,9 +1,20 @@
-﻿using T4Activitats;
+﻿using System;
+using System.Collections;
+namespace T4Activitats { 
+    public class ActT4 {
+        public static void Main() {
+            //Exercici 1
+            //Parella<string, int> parellaStringInt = new Parella<string, int>("text", 1);
+            //Parella<double, string> parellaDoubleString = new Parella<double, string>(3.14, "text");
 
-public class ActT4 {
-    public static void Main() {
-//Exercici 1
-        Parella <string,int> parellaStringInt = new Parella<string, int>("text", 1);
-        Parella<double, string> parellaDoubleString = new Parella<double, string>(3.14, "text");
+            //Exercici 2
+            int listArrayLenght;
+
+            ArrayList arrayList = new ArrayList();
+            listArrayLenght = Utils.AskIntValue();
+            for (int i = 0; i < listArrayLenght; i++) { 
+                arrayList.Add(Console.ReadLine());
+            }
+        }
     }
 }
---
+++ Utils.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T4Activitats {
+    public static class Utils {
+        public static int AskIntValue() {
+            const string Message = "Introdueix un valor enter"
+            int intNum;
+
+            Console.WriteLine(Message);
+            if (Int32.TryParse(Console.ReadLine(), out intNum)) { 
+                return intNum;
+            }
+            return AskIntValue();
+        }
+        public static int AskIntValue(string message) {
+            int intNum;
+
+            Console.WriteLine(message);
+            if (Int32.TryParse(Console.ReadLine(), out intNum)) {
+                return intNum;
+            }
+            return AskIntValue();
+        }
+    }
+}
